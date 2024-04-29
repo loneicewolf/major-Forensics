9a0539dc97d4d3ec5c1a5b3b37ad054eb615d2d2
Will <68499986+loneicewolf@users.noreply.github.com>
2024-04-29 20:33:57 +0200

    Create 1.sh

---
+++ SCRIPTS/1.sh
@@ -0,0 +1,22 @@
+# bash script
+# todo
+# add correct* shebang
+# rename
+# add functions
+# error handling and exceptions
+# correct/good practice/coding
+# think how this should and might get used
+# 
+
+
+# FUNCTIONS
+# [] color (for those with vision problems)
+## like ourselves: cannot easily see yellow/brown so change to RED/PURPLE or BLUE.
+# [] using tools
+# [] input output
+# [] parameters,debug
+
+
+### using these
+# https://github.com/loneicewolf/map-out-1
+# https://embeddedsw.net/
